a836d1a3f6437c74b1e0699aa280bc012c3d0202
Longs3 <senglong0887663213@gmail.com>
2022-09-30 16:40:53 +0700

    class

--- func.py
+++ func.py
@@ -1,29 +1,30 @@
-def sub(a, b):
-	return a-b
-def add(a, b):
-	return a+b
-def div(a, b):
-	return a/b
-def mul(a, b):
-	return a*b
-def floor(a):
-	return int(a//1)
-def ceiling(a):
-	if (a%1==0): return int(a)
-	return int(a//1+1)
-def addCeiling(a, b):
-	return add(ceiling(a), ceiling(b))
-def subCeiling(a, b):
-	return sub(ceiling(a), ceiling(b))
-def mulCeiling(a, b):
-	return mul(ceiling(a), ceiling(b))
-def divCeiling(a, b):
-	return div(ceiling(a), ceiling(b))
-def addFloor(a, b):
-	return add(floor(a), floor(b))
-def subFloor(a, b):
-	return sub(floor(a), floor(b))
-def mulFloor(a, b):
-	return mul(floor(a), floor(b))
-def divFloor(a, b):
-	return div(floor(a), floor(b))
+class Math:
+	def sub(a, b):
+		return a-b
+	def add(a, b):
+		return a+b
+	def div(a, b):
+		return a/b
+	def mul(a, b):
+		return a*b
+	def floor(a):
+		return int(a//1)
+	def ceiling(a):
+		if (a%1==0): return int(a)
+		return int(a//1+1)
+	def addCeiling(a, b):
+		return Math.add(Math.ceiling(a), Math.ceiling(b))
+	def subCeiling(a, b):
+		return Math.sub(Math.ceiling(a), Math.ceiling(b))
+	def mulCeiling(a, b):
+		return Math.mul(Math.ceiling(a), Math.ceiling(b))
+	def divCeiling(a, b):
+		return Math.div(Math.ceiling(a), Math.ceiling(b))
+	def addFloor(a, b):
+		return Math.add(Math.floor(a), Math.floor(b))
+	def subFloor(a, b):
+		return Math.sub(Math.floor(a), Math.floor(b))
+	def mulFloor(a, b):
+		return Math.mul(Math.floor(a), Math.floor(b))
+	def divFloor(a, b):
+		return Math.div(Math.floor(a), Math.floor(b))
