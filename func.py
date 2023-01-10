05f18e88930b22227ee0c1cb2a46eb56a29fe3b1
Longs3 <senglong0887663213@gmail.com>
2023-01-10 10:01:21 +0700

    new update

--- func.py
+++ func.py
@@ -1,47 +1,29 @@
-class Math:
-	def sub(a, b):
-		return a-b
-	def add(a, b):
-		return a+b
-	def div(a, b):
-		return a/b
-	def mul(a, b):
-		return a*b
-	def floor(a):
-		return int(a//1)
-	def ceiling(a):
-		if (a%1==0): return int(a)
-		return int(a//1+1)
-	def addCeiling(a, b):
-		return Math.add(Math.ceiling(a), Math.ceiling(b))
-	def subCeiling(a, b):
-		return Math.sub(Math.ceiling(a), Math.ceiling(b))
-	def mulCeiling(a, b):
-		return Math.mul(Math.ceiling(a), Math.ceiling(b))
-	def divCeiling(a, b):
-		return Math.div(Math.ceiling(a), Math.ceiling(b))
-	def addFloor(a, b):
-		return Math.add(Math.floor(a), Math.floor(b))
-	def subFloor(a, b):
-		return Math.sub(Math.floor(a), Math.floor(b))
-	def mulFloor(a, b):
-		return Math.mul(Math.floor(a), Math.floor(b))
-	def divFloor(a, b):
-		return Math.div(Math.floor(a), Math.floor(b))
+import os
 
-class Bitwise:
-	def lShift(value, unitShift):
-		try:
-			int(unitShift)
-			int(value)
-		except ValueError:
-			print('invalid value')
-			return
-		return value << unitShift
-	def rShift(value, unitShift):
-		try:
-			int(unitShift)
-			int(value)
-		except ValueError:
-			print('invalid value')
-			return
+
+def diskUsage(path):
+    """
+    Finding the usage of a folder or file in a specific location in the drive
+    :param path: The path to the file or folder
+    :return: The size of each file in the folder
+    """
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        for i in os.listdir(path):
+            childPath = os.path.join(path, i)
+            total += diskUsage(childPath)
+    print('{0:<6}'.format(total), path)
+    return total
+
+
+def reverse(array, start, stop):
+    """
+    Reversing an array using recursion
+    :param array:   The array to be reverse
+    :param start:  The starting index
+    :param stop:    The ending index
+    :return: Another recursion until start is equal to stop-1
+    """
+    if start < stop - 1:
+        array[start], array[stop - 1] = array[stop - 1], array[start]
+        reverse(array, start + 1, stop - 1)
